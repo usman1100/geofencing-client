cdf8569a176cee3b2469eaa508776532f376ed34
usman1100 <usmanahmed1100@gmail.com>
2023-11-15 22:37:56 +0500

    drawing circles

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { LngLatLike, Map } from "maplibre-gl";
+import { LngLat, LngLatLike, Map } from "maplibre-gl";
 import { useEffect, useState } from "react";
+import { circleData } from "./data";
 
 const BOX_SIZE = "800px";
 const INIT_CORDS = [69.84381016559087, 31.26276045549733];
 
 function App() {
 	const [map, setMap] = useState<Map | undefined>();
-	const [cords, setCords] = useState<{
-		lng: number;
-		lat: number;
-	}>({
-		lat: INIT_CORDS[0],
-		lng: INIT_CORDS[1],
-	});
 
 	useEffect(() => {
 		setMap(
@@ -25,18 +19,75 @@ function App() {
 		);
 	}, []);
 
-	map?.on("move", () => {
-		const center = map?.getCenter ? map?.getCenter() : null;
+	const drawCircle = (cords: LngLat) => {
+		if (!map) return;
+		map.addSource("circle2", {
+			type: "geojson",
+			// draw a 200km radius circle around the point
+			// cords = {lng, lat}
+			data: {
+				type: "Feature",
+				geometry: {
+					type: "Polygon",
+					coordinates: [
+						[
+							[cords.lng, cords.lat + 0.1],
+							[cords.lng + 0.1, cords.lat],
+							[cords.lng, cords.lat - 0.1],
+							[cords.lng - 0.1, cords.lat],
+							[cords.lng, cords.lat + 0.1],
+							[cords.lng + 0.1, cords.lat],
+						],
+					],
+				},
+			},
+		});
+		map.addLayer({
+			id: "circle2",
+			type: "fill",
+			source: "circle2",
+			layout: {},
+			paint: {
+				"fill-color": "#088",
+				"fill-opacity": 0.8,
+			},
+		});
+		map.redraw();
+	};
 
-		if (center) {
-			setCords(center);
-		}
-	});
+	useEffect(() => {
+		if (!map) return;
+
+		map?.on("load", () => {
+			map.addSource("circle", {
+				type: "geojson",
+				data: circleData,
+			});
+			map.addLayer({
+				id: "circle",
+				type: "fill",
+				source: "circle",
+				layout: {},
+				paint: {
+					"fill-color": "#088",
+					"fill-opacity": 0.8,
+				},
+			});
+		});
+
+		map?.on("click", (e) => {
+			const cords = e.lngLat;
+			console.log(cords);
+			drawCircle(cords);
+		});
+	}, [map]);
 
 	return (
 		<div>
-			<div id="map" style={{ width: BOX_SIZE, height: BOX_SIZE }}></div>
-			<pre>{JSON.stringify(cords, null, 2)}</pre>
+			<div
+				id="map"
+				style={{ width: BOX_SIZE, height: BOX_SIZE, margin: "100px" }}
+			></div>
 		</div>
 	);
 }
